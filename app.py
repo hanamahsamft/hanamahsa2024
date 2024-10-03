5852d79a470d2188a814d7a47e2ea3d2d5fd02f4
hana <hanaiiggg84@gmail.com>
2024-10-03 17:25:38 +0330

    hana

--- app.py
+++ app.py
@@ -4,9 +4,36 @@ app = Flask(__name__)
 
 
 @app.route('/index')
+@app.route('/')
 def index():
     students = session.query(Student).all()
     return render_template('index.html', students=students)
+
+
+@app.route("/details/<int:id>")
+def details(id):
+    s = session.query(Student).get(id)
+    return render_template('details.html', s=s)
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    s = session.query(Student).get(id)
+    session.delete(s)
+    return redirect(url_for('index'))
+
+
+@app.route('/update/<int:id>', methods=['POST', 'GET'])
+def update(id):
+    st = session.query(Student).get(id)
+    if request.method == "POST":
+        n = request.form['name']
+        e = request.form['email']
+        st.name = n
+        st.email = e
+        session.commit()
+        return redirect(url_for('index'))
+    return render_template('update.html', st=st)
 
 
 @app.route('/create', methods=['GET', 'POST'])
